6386756f3e007df2014588790a4a2f0fb4497808
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-10-03 12:02:52 -0700

    feat: update name format

--- backend/app.py
+++ backend/app.py
@@ -15,18 +15,26 @@ app = FastAPI()
 # --- Data Loading and Conversion ---
 
 DATA = pd.read_csv(CSV_FILE_NAME, comment='#')
-# Add a 'name' column to the dataframe
-# If the kepler name exists, use "<kepler_name> (KOI <kepoi_name>)". If only the kepoi exists, use "unnamed (<kepoi_name>)"
-DATA["name"] = DATA[["kepler_name", "kepoi_name"]].apply(lambda x: f"{x['kepler_name']} ({x['kepoi_name']})" if pd.notna(x["kepler_name"]) else f"unnamed ({x['kepoi_name']})", axis=1)
-# assert all names are unique
-assert DATA["name"].nunique() == len(DATA["name"]), "Names are not unique"
 
 @app.get("/exoplanets")
 async def get_exoplanets():
     """
     Endpoint that reads koi.csv and returns the data as a list of JSON objects.
     """
-    return DATA["name"].tolist()
+    return [
+        {
+            "name": record.kepler_name,
+            "id": record.kepoi_name,
+        }
+        for record in DATA.fillna("").itertuples()
+    ]
+
+@app.get("/exoplanets/{kepoi_name}")
+async def get_exoplanet(kepoi_name: str):
+    """
+    Endpoint that reads koi.csv and returns the data as a list of JSON objects.
+    """
+    return json.dumps(DATA[DATA["kepoi_name"] == kepoi_name].to_dict(orient='records'))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
