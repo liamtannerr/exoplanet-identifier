c94c778488572734025c95c54ffef897dfca7ec7
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-10-03 12:04:16 -0700

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Exoplanet Identifier";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python311Packages.fastapi
+            python311Packages.uvicorn
+            python311Packages.pandas
+          ];
+        };
+      }
+    );
+}
