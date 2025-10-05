e4721786a02fc529f77632da23622b5dc7a0025c
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-10-05 14:08:35 -0700

    fix: model input

--- backend/app.py
+++ backend/app.py
@@ -14,26 +14,8 @@ app = FastAPI()
 
 # read csv
 DATA = pd.read_csv(CSV_FILE_NAME, comment='#')
-# fill na with empty string if column is string, else 0
-for column in DATA.columns:
-    if DATA[column].dtype == 'object':
-        DATA[column] = DATA[column].fillna("")
-    else:
-        DATA[column] = DATA[column].fillna(0)
 # create orbital radius column
 DATA["orbital_radius"] = DATA["koi_dor"] * DATA["koi_srad"]
-# rename columns
-DATA = DATA.rename(columns={
-    "kepoi_name": "kepoi_name",
-    "kepler_name": "kepler_name",
-    "koi_period": "orbital_period",
-    "koi_prad": "planet_radius",
-    "koi_srad": "stellar_radius",
-    "koi_teq": "temperature",
-    "koi_steff": "stellar_temperature",
-})
-# select only required columns
-# DATA = DATA[["kepoi_name", "kepler_name", "orbital_period", "planet_radius", "stellar_radius", "temperature", "stellar_temperature", "orbital_radius"]]
 
 origins = [
     "http://localhost:3000",
@@ -78,15 +60,25 @@ async def get_exoplanet_metrics(kepoi_name: List[str] = Query(default=[])):
     Endpoint that reads koi.csv and returns the data as a list of JSON objects.
     """
     data = DATA[DATA["kepoi_name"].astype(str).isin(kepoi_name)].copy()
+    predictions = [predict_row(record) for record in data.dropna(axis=1).to_dict(orient='records')]
+    for column in data.columns:
+        if data[column].dtype == 'object':
+            data[column] = data[column].fillna("")
+        else:
+            data[column] = data[column].fillna(0)
     data = data.to_dict(orient='records')
-    predictions = [
-        {
-            "is_exoplanet": predict["is_candidate"],
-            "is_exoplanet_confidence": predict["confidence"],
-        }
-        for predict in (predict_row(record) for record in data)
-    ]
-    data = [ExoplanetMetrics(**record, **prediction) for record, prediction in zip(data, predictions)]
+    data = [ExoplanetMetrics(
+        kepoi_name=record["kepoi_name"],
+        kepler_name=record["kepler_name"],
+        orbital_period=record["koi_period"],
+        planet_radius=record["koi_prad"],
+        stellar_radius=record["koi_srad"],
+        orbital_radius=record["koi_dor"] * record["koi_srad"],
+        temperature=record["koi_teq"],
+        stellar_temperature=record["koi_steff"],
+        is_exoplanet=prediction["is_candidate"],
+        is_exoplanet_confidence=prediction["confidence"],
+    ) for record, prediction in zip(data, predictions)]
     return data
 
 if __name__ == "__main__":
